3ebfaee01c3f78371f10311ece9450e411eee9b4
Tito Bottitta <tito@upstatement.com>
2015-04-28 22:28:00 -0400

    Added autoprefixer to base config file

--- config.rb
+++ config.rb
@@ -18,3 +18,14 @@ line_comments = false
 # output_style = :compact :compressed :nested :expanded
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
+
+# Add Autoprefixer Support
+# https://github.com/ai/autoprefixer#usage
+require 'autoprefixer-rails'
+
+on_stylesheet_saved do |file|
+  css = File.read(file)
+  File.open(file, 'w') do |io|
+    io << AutoprefixerRails.process(css)
+  end
+end
